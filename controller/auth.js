c8560c65913cc02be2cdebe2959d4adf6ecc101c
AfriVacx <166631869+AfriVacx@users.noreply.github.com>
2024-04-16 01:27:02 +0100

    Update auth.js

--- controller/auth.js
+++ controller/auth.js
@@ -170,125 +170,6 @@ module.exports.userLogin = async (req, res, next) => {
   }
 };
 
-/******************
- * Password Reset *
- ******************/
-module.exports.passwordReset = async (req, res, next) => {
-  const email = req.body.email;
-
-  try {
-    // Check if user exists with email
-    const user = await User.findOne(
-      { "details.email": email },
-      "details resetToken resetExpiration"
-    );
-
-    // Check for validation errors
-    const validatorErrors = validationResult(req);
-    error.validationError(validatorErrors, res);
-
-    // Check if user is undefined
-    if (!user) error.errorHandler(res, "No user found with email", "email");
-
-    // Continue if there are no errors
-
-    // Generate random reset token
-    const resetToken = await crypto.randomBytes(32).toString("hex");
-
-    // Calculate passwordExpiration
-    const resetExpiration = Date.now() + 3600000;
-
-    // Update found user object
-    user.resetToken = resetToken;
-    user.resetExpiration = resetExpiration;
-
-    // Send password reset mail to user
-
-    // Save user updates to database
-    await user.save();
-
-    // Send response to client
-    res.status(200).json({
-      message: "A password reset link has been sent to your email",
-      type: "password reset",
-      resetToken,
-    });
-  } catch (err) {
-    error.error(err);
-  }
-};
-
-/***********************
- * Get Password Token *
- ***********************/
-module.exports.getPasswordToken = async (req, res, next) => {
-  const email = req.params.email;
-
-  try {
-    // Check for matching token on a user
-    const user = await User.findOne(
-      { "details.email": email },
-      "resetToken resetExpiration"
-    );
-
-    // Check if user is undefined
-    if (!user) error.errorHandler(res, "Invalid user", "token");
-
-    // Check if token has expired
-    if (user.resetExpiration < Date.now()) {
-      // Clear user resetToken and expiration
-      user.resetToken = undefined;
-      user.resetExpiration = undefined;
-
-      // Save user to database
-      await user.save();
-      error.errorHandler(res, "password reset session has expired", "message");
-    }
-
-    res.status(200).json({ token: user.resetToken });
-  } catch (err) {
-    error.error(err, next);
-  }
-};
-
-/******************
- * Password Change *
- ******************/
-module.exports.passwordChange = async (req, res, next) => {
-  const resetToken = req.params.resetToken,
-    password = req.body.password;
-
-  try {
-    // Get user
-    const user = await User.findOne({ resetToken }, "password resetToken");
-
-    // Check if user is undefined
-    if (!user) error.errorHandler(res, "No user found", "user");
-
-    // Continue if there are no errors
-
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    // Assign new password to user
-    user.password = hashedPassword;
-
-    // Remove resetToken/Expiration
-    user.resetToken = undefined;
-    user.resetExpiration = undefined;
-
-    //  Save user changes to database
-    await user.save();
-
-    // Send response back to client
-    res
-      .status(201)
-      .json({ message: "password successfully changed", type: "password" });
-  } catch (err) {
-    error.error(err, next);
-  }
-};
-
 /*****************
  * Get all Users *
  *****************/
@@ -311,16 +192,15 @@ module.exports.getUsers = async (req, res, next) => {
  * try something *
  *****************/
 module.exports.something = async (req, res, next) => {
-  const userId = req.body.userId;
   try {
     // Get user
-    const user = await User.findById(userId);
+    const users = await Board.find();
 
     // Check if user is undefined
-    if (!user) error.errorHandler(res, "No user found", "user");
+    if (!users) error.errorHandler(res, "No user found", "user");
 
     // Send response back to client
-    res.status(201).json({ message: "users fetched", user });
+    res.status(201).json({ message: "users fetched", users });
   } catch (err) {
     error.error(err, next);
   }
